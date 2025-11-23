12eec8d88c8365445a3120a4feda6c086f43d363
Surajsurya95096 <surajroxofficial01@gmail.com>
2025-11-23 14:43:32 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -98,7 +98,7 @@ async def start(client, message):
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
     if len(message.command) != 2:
         buttons = [[
-                    InlineKeyboardButton('🔰 ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ 🔰', url=f'http://telegram.me/{temp.U_NAME}?startgroup=true')
+                    InlineKeyboardButton('𓆩✙ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ ✙𓆪', url=f'http://telegram.me/{temp.U_NAME}?startgroup=true')
                 ],[
                     InlineKeyboardButton(' Sᴇᴀʀᴄʜ 🎬', callback_data='help'),
                     InlineKeyboardButton(' ᴀʙᴏᴜᴛ 🌝', callback_data='about')
@@ -129,7 +129,7 @@ async def start(client, message):
 
     if len(message.command) == 2 and message.command[1] in ["subscribe", "error", "okay", "help"]:
         buttons = [[
-                    InlineKeyboardButton('🔰 ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ 🔰', url=f'http://telegram.me/{temp.U_NAME}?startgroup=true')
+                    InlineKeyboardButton('𓆩✙ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ ✙𓆪', url=f'http://telegram.me/{temp.U_NAME}?startgroup=true')
                 ],[
                     InlineKeyboardButton(' Sᴇᴀʀᴄʜ 🎬', callback_data='help'),
                     InlineKeyboardButton(' ᴀʙᴏᴜᴛ 🌝', callback_data='about')
